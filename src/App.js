f58d7b5308223689d932bb46611d4bfd127580a9
Lucas <lucasfernando992@gmail.com>
2021-06-05 10:02:04 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+import FeatureMovie from './components/FeatureMovie';
+import Header from './components/Header';
+
+export default () =>{
+
+  const [movieList, setMovieList] = useState([]);
+  const [featureData, setFeatureData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+
+  useEffect(()=>{
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeatureData(chosenInfo);
+    };
+
+    loadAll();
+  }, []);
+
+  useEffect(()=>{
+    const scrollListener = () => {
+      if(window.scrollY > 10){
+        setBlackHeader(true);
+      }else{
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener);
+
+    return () =>{
+      window.removeEventListener('scroll', scrollListener);
+    }
+  },[])
+
+  return(
+    <div className="page">
+
+      <Header black={blackHeader} />
+
+      {featureData &&
+        <FeatureMovie item={featureData} />
+      }
+
+      <section className="lists">
+        {movieList.map((item, key)=>(
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+
+      <footer>
+        Feito com <span role="img" aria-label="coração">♥</span> por Lucas Rodrigues <br />
+        Direitos de imagem para Netflix<br />
+        Dados pegos do site Themoviedb.org
+      </footer>
+
+      {movieList.length <=0 &&
+        <div className="loading">
+          <img src="https://media.wired.com/photos/592744d3f3e2356fd800bf00/master/w_2560%2Cc_limit/Netflix_LoadTime.gif" alt="Carregando" />
+        </div>
+      }
+      
+    </div>
+  )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+export default ({black}) =>{
+    return (
+        <header className={black ? 'black' : ''}>
+            <div className="header--logo">
+                <a href="/">
+                    <img src="https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-1-1.png" alt="netflix" />
+                </a>
+            </div>
+            <div className="header--user">
+                <a href="/">
+                    <img src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="Usuário" />
+                </a>
+            </div>
+        </header>
+    );
+}
